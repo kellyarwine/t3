ecbd34294be8310818d8ec2c27603aa7dba4a311
Kelly Steensma <kellysteensma@gmail.com>
2013-03-06 12:00:17 -0600

    Cleaned up initialization of variables and deleted the unnessecary ones like score.

--- lib/t3/minimax.rb
+++ lib/t3/minimax.rb
@@ -12,48 +12,47 @@ module T3
       require 'pry'
       require 'pry-debugger'  
 
-      depth = 0
-      score =0
-      move_history = Hash.new
       score_hash = Hash.new
 
-      for i in 0..available_spaces.length-1
+      available_spaces.each_with_index do |available_space,i|
         # binding.pry
         space = available_spaces[i]
-        score_hash[space] = run_scorer(@board.available_spaces,gamepieces,score,depth)
-        # result = 0
+        score_hash[space] = run_scorer(@board.available_spaces,gamepieces)
       end
       score_hash
     end
   
-    def run_scorer(available_spaces,gamepieces,score,depth,p1=nil,p2=nil,p3=nil)
+    def run_scorer(available_spaces,gamepieces,depth=0,p1=nil,p2=nil,p3=nil)
       score_hash = Hash.new
-      move_history = Hash.new
       result = 0
-      for i in 0..available_spaces.length-1
 
+      available_spaces.each_with_index do |available_space, i|
+        move_history = Hash.new
         depth += 1
         position = @board.spaces.index(available_spaces[i])
         @board.spaces[position] = gamepieces.first
         move_history[position+1] = gamepieces.first
-        # binding.pry
-        result += run_scorer(@board.available_spaces,gamepieces.reverse,score,depth,move_history,p1,p2) if not @game_rules.game_over?
-        result += score_win(score,depth)
-        puts "#{result} => #{p3} #{p2} #{p1} #{move_history}"
-        # binding.pry
-        # space = available_spaces[i]
-        # score_hash[space] = result.round(3)
-        # binding.pry
+
+        result += run_scorer(@board.available_spaces,gamepieces.reverse,depth,move_history,p1,p2) if not @game_rules.game_over?
+        
+        result += score_win(depth)
+        
+        space = available_spaces[i]
+        score_hash[space] = result
+        
+        puts "History:     #{result} => #{p3} #{p2} #{p1} #{move_history}"
+        puts "Score Hash:  #{result} => #{score_hash}"
+        # binding.pry if space == "6" || space == "7"
    
         unwind_board(move_history)
 
-        move_history = Hash.new
+        # move_history = Hash.new
         depth -= 1
       end
         result
     end
   
-    def score_win(score,depth)
+    def score_win(depth)
       case @game_rules.winning_gamepiece
       when nil then 0.0
       when "o" then 1.0/depth
