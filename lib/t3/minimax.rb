5dafaeef0cdfcef9f1a01da73382e4090dffe6b6
Kelly Steensma <kellysteensma@gmail.com>
2013-03-05 22:06:52 -0600

    Working Minimax returning hash of space and score.  Score is calculated by taking +1/0/-1 dividing by depth and then summing those all up for one starting space.

--- lib/t3/minimax.rb
+++ lib/t3/minimax.rb
@@ -10,37 +10,84 @@ module T3
   
     def feed_scorer(available_spaces,gamepieces,score)
       require 'pry'
+      require 'pry-debugger'  
+      # binding.pry
       move_history = Hash.new
-      score_array = Array.new
+      score_hash = Hash.new
+      depth = 0
+      result = 0
       for i in 0..available_spaces.length-1
         # binding.pry
+        depth += 1
         position = @board.spaces.index(available_spaces[i])
         @board.spaces[position] = gamepieces.first
         move_history[position+1] = gamepieces.first
+        
+        result = run_scorer(available_spaces-[available_spaces[i]],gamepieces.reverse,score,depth,move_history) if not @game_rules.game_over?
+        
         # binding.pry
-        score_array << run_scorer(available_spaces-[available_spaces[i]],gamepieces.reverse,score,move_history)
+        result += score_win(score,depth)
+        space = available_spaces[i]
+        score_hash[space] = result.round(3)
+        # score_array << result.round(3)
+          if @game_rules.win_game?
+            puts "----------------------------------------------\n"
+          print move_history
+          print "\n"          
+            print "#{result}"
+            print "\n"
+            puts "#{@game_rules.winning_gamepiece} wins!\n"
+            puts "----------------------------------------------\n"
+          end
    
         reset_board_and_counters(move_history)
         move_history = Hash.new
-
+        result = 0
+        depth -= 1
       end
-      print "This is the passed score: #{score_array}"
-      score_array
+      score_hash
     end
   
-    def run_scorer(available_spaces,gamepieces,score,p1 = nil,p2 = nil,p3 = nil)
-
+    def run_scorer(available_spaces,gamepieces,score,depth,p1 = nil,p2 = nil,p3 = nil)
       move_history = Hash.new
-
+      result = 0
       for i in 0..available_spaces.length-1
+        # binding.pry
+        depth += 1
         position = @board.spaces.index(available_spaces[i])
         @board.spaces[position] = gamepieces.first
         move_history[position+1] = gamepieces.first
         
 
-        puts "----------------------------------------------\n"  
-        print "for #{i} in 0..#{available_spaces.length-1}"
-        print "\n"
+        # puts "----------------------------------------------\n"  
+        # print "for #{i} in 0..#{available_spaces.length-1}"
+        # print "\n"
+        # if p3
+        #   print "#{p3}"
+        #   print "\n"
+        # end
+        # if p2
+        #   print "#{p2}"
+        #   print "\n"
+        # end
+        # if p1
+        #   print "#{p1}"
+        #   print "\n"
+        # end
+        # print move_history
+        # print "\n"
+        # print @board.spaces
+        # print "\n"
+        
+      # binding.pry
+        
+        result += run_scorer(@board.available_spaces,gamepieces.reverse,score,depth,move_history,p1,p2) if not @game_rules.game_over?
+
+
+        # binding.pry
+        result += score_win(score,depth)
+        if @game_rules.win_game?
+          puts "----------------------------------------------\n"
         if p3
           print "#{p3}"
           print "\n"
@@ -54,38 +101,36 @@ module T3
           print "\n"
         end
         print move_history
-        print "\n"
-        print @board.spaces
-        print "\n"
-        
-        score = run_scorer(@board.available_spaces,gamepieces.reverse,score,move_history,p1,p2) if not @game_rules.game_over?
-        # binding.pry
-        score = score_win(score)
-        # binding.pry
-        if @game_rules.win_game?
-          puts "----------------------------------------------\n"
-          print "#{score}"
+        print "\n"          
+          print "#{result}"
           print "\n"
           puts "#{@game_rules.winning_gamepiece} wins!\n"
           puts "----------------------------------------------\n"
         end
 
         reset_board_and_counters(move_history)
+        # binding.pry
+        move_history = Hash.new
+        depth -= 1
       end
-        score
+        result
     end
   
-    def score_win(score)
+    def score_win(score,depth)
+      # binding.pry
       if @game_rules.win_game? == false
-        score += 0
+        score += 0.0
       elsif @game_rules.winning_gamepiece == "o"
-        score += 1
+        score += 1.0
+        score/depth
       else
-        score -= 1
+        score -= 1.0
+        score/depth
       end
     end
   
     def reset_board_and_counters(move_history)
+      # binding.pry
       move_history.keys.each do |key|
         board_space_value = (key).to_s
         @board.spaces[key-1] = board_space_value
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -12,12 +12,24 @@ describe T3::Minimax do
                             # "x","o","6",
                             # "7","8","9"]
 
-                            # 6 7     => x wins
+                            # 6 7     => x wins => -.5
                             # 6 8 7 9 => draw
-                            # 6 8 9 7 => x wins
+                            # 6 8 9 7 => x wins => -.25
                             # 6 9 7 8 => draw
-                            # 6 9 8   => o wins                            
+                            # 6 9 8   => o wins => .333
+                            # 7 6 8   => o wins => .333
+                            # 7 6 9 8 => draw
+                            # 7 8 6 9 => draw
+                            # 7 8 9 6 => draw
+                            # 7 9 8   => o wins => .333
+                            # 7 9 6 8 => draw
+                            # 8       => o wins => 1.0
+                            # 9 6 7 8 => draw
+                            # 9 6 8   => o wins => .333
+                            # 9 7     => x wins => -.5
+                            # 9 8 6 7 => x wins => -.25
+                            # 9 8 7 6 => draw
 
-    subject.feed_scorer(board.available_spaces,["o","x"],0).should == [-1,2,1,-1]
+    subject.feed_scorer(board.available_spaces,["o","x"],0.0).should == [-0.417,0.667,1.0,-0.417]
   end
 end
